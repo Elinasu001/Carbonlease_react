8aa951cf5252cde21f40aacd06f109abc55e2894
suelina <suelina001@gmail.com>
2025-12-19 17:47:46 +0900

    ,

--- carbonlease_react/carbonlease/vite.config.js
+++ carbonlease_react/carbonlease/vite.config.js
@@ -4,11 +4,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  
-=======
   define: {
     global: {},
   },
->>>>>>> 25cd6adc4f348c767f8b4f611ad4b8831b256173
 })
